1cbe0444af5898704ba044b7d5c5878f23b48997
Arique Aguilar <arique1104@gmail.com>
2020-06-06 16:20:11 -0500

    Key_feature object and Today feature object

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,3 +1,12 @@
 class Offset
+attr_reader :key_feature,
+            :today
+  def initialize(key_feature, today)
+    @key_feature = key_feature
+    @today = today
+  end
+
+
+
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -3,7 +3,7 @@ require "minitest/pride"
 require "mocha/minitest"
 require "./lib/key"
 require "./lib/key_feature"
-require "./.lib/today_date"
+require "./lib/today_date"
 require "./lib/offset"
 
 class OffsetTest < Minitest::Test
@@ -12,7 +12,23 @@ class OffsetTest < Minitest::Test
     key = Key.new
     key.generate_five_digit_key
     key_feature = KeyFeature.new(key)
-    offset = Offset.new(key_feature)
+    today = TodayDate.new
+    today.right_now
+    today.timestamp
+    offset = Offset.new(key_feature, today)
     assert_instance_of Offset, offset
+  end
+
+  def test_it_has_attributes
+    key = Key.new
+    key.generate_five_digit_key
+    key_feature = KeyFeature.new(key)
+    today = TodayDate.new
+    today.right_now
+    today.timestamp
+    offset = Offset.new(key_feature, today)
+
+    assert_equal key_feature, offset.key_feature
+    assert_equal today, offset.today
   end
 end
