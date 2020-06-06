87f9e3f720b0f4e47c2b685d979389b84a338d56
Arique Aguilar <arique1104@gmail.com>
2020-06-06 16:09:26 -0500

    Offset test it exists test and require lib/today_date

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -3,11 +3,16 @@ require "minitest/pride"
 require "mocha/minitest"
 require "./lib/key"
 require "./lib/key_feature"
+require "./.lib/today_date"
 require "./lib/offset"
 
 class OffsetTest < Minitest::Test
 
   def test_it_exists
-
+    key = Key.new
+    key.generate_five_digit_key
+    key_feature = KeyFeature.new(key)
+    offset = Offset.new(key_feature)
+    assert_instance_of Offset, offset
   end
 end
