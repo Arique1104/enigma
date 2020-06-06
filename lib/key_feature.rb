2a88d658523f8b50c7acabab16b21d30b10172e1
Arique Aguilar <arique1104@gmail.com>
2020-06-06 14:14:49 -0500

    Passing KeyFeature code

--- lib/key_feature.rb
+++ lib/key_feature.rb
@@ -4,14 +4,10 @@ class KeyFeature
               :c_key,
               :d_key
   def initialize(key)
-    @a_key = "12"
-    @b_key = "23"
-    @c_key = "34"
-    @d_key = "45"
-    # @a_key = "#{key.five_digit_key[0]}#{key.five_digit_key[1]}"
-    # @b_key = "#{key.five_digit_key[1]}#{key.five_digit_key[2]}"
-    # @c_key = "#{key.five_digit_key[2]}#{key.five_digit_key[3]}"
-    # @d_key = "#{key.five_digit_key[3]}#{key.five_digit_key[4]}"
+    @a_key = "#{key.five_digit_key[0]}#{key.five_digit_key[1]}"
+    @b_key = "#{key.five_digit_key[1]}#{key.five_digit_key[2]}"
+    @c_key = "#{key.five_digit_key[2]}#{key.five_digit_key[3]}"
+    @d_key = "#{key.five_digit_key[3]}#{key.five_digit_key[4]}"
 
   end
 
