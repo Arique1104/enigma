b8fe1185f8bfb223dbd14cd8f3055e8b28856f07
Arique Aguilar <arique1104@gmail.com>
2020-06-06 15:50:15 -0500

    Right_now method test and passing code

--- lib/today_date.rb
+++ lib/today_date.rb
@@ -1,7 +1,15 @@
+require "date"
+
 class TodayDate
  attr_reader :date_initiated
   def initialize
     @date_initiated = []
   end
+
+  def right_now
+    now = Time.now
+    now_formatted = now.strftime("%e%m%y")
+  end
+
 
 end
--- test/today_date_test.rb
+++ test/today_date_test.rb
@@ -17,4 +17,10 @@ class TodayDateTest < Minitest::Test
     assert_equal [], today.date_initiated
   end
 
+  def test_it_can_get_today_date
+    today = TodayDate.new
+
+    assert_equal " 60620", today.right_now
+  end
+
 end
