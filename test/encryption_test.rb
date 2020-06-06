531cc91c49a119942b617291804269a88862989f
Arique Aguilar <arique1104@gmail.com>
2020-06-06 18:36:13 -0500

    Add encryption test and class file and include all requireds on top of encryption test

---
+++ test/encryption_test.rb
@@ -0,0 +1,20 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/key"
+require "./lib/key_feature"
+require "./lib/today_date"
+require "./lib/offset_calculations"
+require "./lib/offset"
+require "./lib/encryption"
+
+class EncryptionTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
