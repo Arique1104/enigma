d899075fbec9e75fbdf417d0bd1c123bcbb44991
Arique Aguilar <arique1104@gmail.com>
2020-06-06 14:13:35 -0500

    Instead of hard coding the key_feature class, I hard coded the origin of the random numbers, the Key class.

--- lib/key.rb
+++ lib/key.rb
@@ -5,11 +5,14 @@ class Key
   end
 
   def generate_five_digit_key
-    5.times do
-      num = rand(0..9)
-      @five_digit_key << num
-    end
-    # require "pry"; binding.pry
+    # 5.times do
+    #   num = rand(0..9)
+      @five_digit_key << 1
+      @five_digit_key << 2
+      @five_digit_key << 3
+      @five_digit_key << 4
+      @five_digit_key << 5
+    # end
   end
 
 
