15e1d5cfde1419a109e90370995afad0772cdbb0
Arique Aguilar <arique1104@gmail.com>
2020-06-06 14:38:30 -0500

    empty array for today_date

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,7 +1,7 @@
 class Offset
- #attr_reader
+ attr_reader :today_date
   def initialize(key_feature)
-
+    @today_date = []
   end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -16,4 +16,13 @@ class OffsetTest < Minitest::Test
     assert_instance_of Offset, offset
 
   end
+
+  def test_it_has_attributes
+    key = Key.new
+    key.generate_five_digit_key
+    key_feature = KeyFeature.new(key)
+    offset = Offset.new(key_feature)
+
+    assert_equal [], offset.today_date
+  end
 end
