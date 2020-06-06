d56c32aee926cbf9c839c60f6222e874e980bb78
Arique Aguilar <arique1104@gmail.com>
2020-06-06 15:13:30 -0500

    Add required files to DateTest

---
+++ test/date_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/key"
+require "./lib/key_feature"
+require "./lib/date"
+
+class DateTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
