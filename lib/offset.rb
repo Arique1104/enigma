c9134d01804532cd7e9dd7a1d9bbde70eda0192b
Arique Aguilar <arique1104@gmail.com>
2020-06-06 13:08:31 -0500

    Realizing that I want my keys to be an object that I can pass through to Offset instead of an instance variable I'm trying to pass through class.

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,8 +1,16 @@
 class Offset
  attr_reader :date_today
-  def initialize
+  def initialize(key_feature_object)
     @date_today = []
+    @a_key = key_feature_object.a_key
+    @b_key = key_feature_object.b_key
+    @c_key = key_feature_object.c_key
+    @d_key = key_feature_object.d_key
 
+  end
+
+  def add_key
+require "pry"; binding.pry
   end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -7,13 +7,34 @@ require "./lib/offset"
 class OffsetTest < Minitest::Test
 
   def test_it_exists
-    offset = Offset.new
+    key_feature = KeyFeature.new
+    key_feature.generate_random_key_numbers
+    key_feature.assign_keys_numbers
+    offset = Offset.new(key_feature)
     assert_instance_of Offset, offset
   end
 
   def test_it_has_attributes
-    offset = Offset.new
+    key_feature = mock
+    key_feature.stub(:generate_random_key_numbers).return([1, 2, 3, 4, 5])
+    key_feature.assign_keys_numbers
+    offset = Offset.new(key_feature)
     assert_equal [], offset.date_today
+
+    assert_equal @a_key, key_feature.a_key
+    assert_equal @b_key, key_feature.b_key
+    assert_equal @c_key, key_feature.c_key
+    assert_equal @d_key, key_feature.d_key
+
+
+  end
+
+  def test_it_can_calculate_today_date
+    key_feature = KeyFeature.new
+    key_feature.generate_random_key_numbers
+    key_feature.assign_keys_numbers
+    offset = Offset.new(key_feature)
+    # require "pry"; binding.pry
   end
 
 end
