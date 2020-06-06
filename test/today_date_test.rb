cb9e5037ac9a0da5af2346d6e6539a10b6f9189f
Arique Aguilar <arique1104@gmail.com>
2020-06-06 15:18:45 -0500

    Required files in TodayDate Test

---
+++ test/today_date_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/key"
+require "./lib/key_feature"
+require "./lib/today_date"
+
+class TodayDateTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
