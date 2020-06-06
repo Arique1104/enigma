0b92b71f2406c8a67d9c334d601bcb4e7e5c0e8b
Arique Aguilar <arique1104@gmail.com>
2020-06-06 17:50:18 -0500

    Final a-d key attribute for offset class, currently set to 0

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,10 +1,18 @@
 class Offset
-attr_reader :key_feature
+attr_reader :key_feature,
+:calculations,
+:final_a_key,
+:final_b_key,
+:final_c_key,
+:final_d_key
 
   def initialize(key_feature, offset_calculations)
     @key_feature = key_feature
     @calculations = offset_calculations
-
+    @final_a_key = 0
+    @final_b_key = 0
+    @final_c_key = 0
+    @final_d_key = 0
   end
 
 
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -3,8 +3,8 @@ require "minitest/pride"
 require "mocha/minitest"
 require "./lib/key"
 require "./lib/key_feature"
-require "./lib/offset_calculations"
 require "./lib/today_date"
+require "./lib/offset_calculations"
 require "./lib/offset"
 
 class OffsetTest < Minitest::Test
@@ -20,5 +20,25 @@ class OffsetTest < Minitest::Test
     offset_calculations.get_last_four
     offset = Offset.new(key_feature, offset_calculations)
     assert_instance_of Offset, offset
+  end
+
+  def test_it_has_attributes
+    key = Key.new
+    key.generate_five_digit_key
+    key_feature = KeyFeature.new(key)
+    today = TodayDate.new
+    today.right_now
+    today.timestamp
+    offset_calculations = OffsetCalculations.new(today)
+    offset_calculations.get_last_four
+    offset = Offset.new(key_feature, offset_calculations)
+
+    assert_equal key_feature, offset.key_feature
+    assert_equal offset_calculations, offset.calculations
+    assert_equal 0, offset.final_a_key
+    assert_equal 0, offset.final_b_key
+    assert_equal 0, offset.final_c_key
+    assert_equal 0, offset.final_d_key
+
   end
 end
